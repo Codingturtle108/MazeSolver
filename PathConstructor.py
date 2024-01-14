b6f8e5812ca34cc8ac42935923a4fda4cd6fec17
Codingturtle108 <black9lightning9@gmail.com>
2024-01-14 20:30:17 +0530

    Output Images Saved for bfs Algorithm

--- PathConstructor.py
+++ PathConstructor.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 def constructor(path):
-    Maze_Solved = cv.imread('TestMazes/tiny.png')
+    Maze_Solved = cv.imread('TestMazes/normal.png')
     cv.namedWindow('Maze_Solved', cv.WINDOW_NORMAL)
     for x,y in path:
         Maze_Solved[y, x] = [0, 0, 255]
+    cv.imwrite('Output bfs/normalsolved.png',Maze_Solved)
     cv.imshow('Maze_Solved', Maze_Solved)
     cv.waitKey(0)
